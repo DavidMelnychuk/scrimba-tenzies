2dc6e8aebf9b16ba3ff84ddf221c34824fcd7a72
David <DavidMelnychuk@users.noreply.github.com>
2023-02-28 14:53:34 -0800

    finish basic tenzies

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,74 @@
 import "./App.css";
+import Die from "./components/Die";
+import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
+import Confetti from "./components/Confetti";
 
-function App() {
-  return <main></main>;
+export default function App() {
+  const [tenzies, setTenzies] = useState(false);
+  const [dice, setDice] = useState(() => allNewDice());
+
+  useEffect(() => {
+    const firstDieValue = dice[0].value;
+    const hasWon = dice.every(
+      (die) => die.isHeld && die.value === firstDieValue
+    );
+
+    if (hasWon) {
+      setTenzies(true);
+    }
+  }, [dice]);
+
+  function allNewDice() {
+    return Array.from({ length: 10 }, () => ({
+      id: nanoid(),
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+    }));
+  }
+
+  const diceElements = dice.map((die) => (
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
+  ));
+
+  function rollNewDice() {
+    if (tenzies) {
+      setTenzies(false);
+      setDice(allNewDice());
+    } else {
+      setDice((oldDice) =>
+        oldDice.map((die) =>
+          die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }
+        )
+      );
+    }
+  }
+
+  function holdDice(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) =>
+        die.id === id ? { ...die, isHeld: !die.isHeld } : die
+      )
+    );
+  }
+
+  return (
+    <main>
+      {tenzies && <Confetti />}
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-button" onClick={rollNewDice}>
+        {tenzies ? "New Game" : "Roll"}
+      </button>
+    </main>
+  );
 }
-
-export default App;
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,7 +1,13 @@
-import "./App.css";
+import "../App.css";
 
-function App() {
-  return <main></main>;
+export default function Die({ id, value, isHeld, holdDice }) {
+  const styles = {
+    backgroundColor: isHeld ? "#59e391" : "white",
+  };
+
+  return (
+    <div className="die" style={styles} onClick={holdDice}>
+      <h2>{value}</h2>
+    </div>
+  );
 }
-
-export default App;
